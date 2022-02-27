b61a3d93dcbf3c0429ad571b6ad850e5ec192b4d
Callum James Gill <CallumPersonal@Callums-iMac.local>
2022-02-27 10:17:54 -0800

    Added in some blue to profile page

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -25,7 +25,7 @@ const Dashboard = () => {
                         </svg>
                     </div>
                 </section>
-                <section className="relative py-16 bg-gray-300">
+                <section className="relative py-16 bg-blue-300">
                     <div className="container mx-auto px-4">
                         <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
                             <div className="px-6">
