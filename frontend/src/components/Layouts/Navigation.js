1b3331c4e258b15d7710793862e99a1325efbc57
Callum James Gill <CallumPersonal@Callums-iMac.local>
2022-02-27 10:22:52 -0800

    Added in the search box

--- frontend/src/components/Layouts/Navigation.js
+++ frontend/src/components/Layouts/Navigation.js
@@ -54,6 +54,16 @@ const Navigation = ({ user }) => {
                             </NavLink>
                         </div>
                     </div>
+                    
+                    <div class="flex justify-center">
+                        <div class="mb-3  mt-3 xl:w-100">
+                            <div class="input-group relative flex flex-wrap items-stretch w-full mb-4 rounded">
+                                <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search Books" aria-label="Search" aria-describedby="button-addon2"></input>
+                                <span class="input-group-text flex items-center px-3 py-1.5 text-base font-normal text-gray-700 text-center whitespace-nowrap rounded" id="basic-addon2">
+                                </span>
+                            </div>
+                        </div>
+                    </div>
 
                     {/* Settings Dropdown */}
                     <div className="hidden sm:flex sm:items-center sm:ml-6">
