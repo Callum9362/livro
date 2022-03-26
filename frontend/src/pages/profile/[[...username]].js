e198fa816adfa4559281c539c70e3855e0b83b93
Callum James Gill <CallumPersonal@Callums-iMac.local>
2022-03-26 10:59:24 -0700

    Restored

--- frontend/src/pages/profile/[[...username]].js
+++ frontend/src/pages/profile/[[...username]].js
@@ -68,7 +68,7 @@ const Dashboard = ({ profileInfo }) => {
                                     <div className="w-full lg:w-4/12 px-4 lg:order-1">
                                         <div className="flex justify-center py-4 lg:pt-4 pt-8">
                                             <div className="mr-4 p-3 text-center">
-                                                <span className="text-xl block uppercase tracking-wide text-gray-700">
+                                                <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
                                                 22
                                                 </span>
                                                 <span className="text-sm text-gray-500">Ratings</span>
