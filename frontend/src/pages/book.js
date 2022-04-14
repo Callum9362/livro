dc112dfad22370c57ee9002a84965d1855b888c0
Callum Gill <callum.gill.1993.cg@gmail.com>
2022-04-14 15:14:57 -0700

    Frontend homework

--- frontend/src/pages/book.js
+++ frontend/src/pages/book.js
@@ -18,7 +18,7 @@ const Book = () => {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                            All the information about the book.
+                            All the information about a single book will display here.
                         </div>
                     </div>
                 </div>
--- frontend/src/pages/books.js
+++ frontend/src/pages/books.js
@@ -18,7 +18,7 @@ const Books = () => {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                            Your books will display here.
+                            A list of books will display here.
                         </div>
                     </div>
                 </div>
