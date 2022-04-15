259bc842813a03a11655ddae1a20346630c231a9
Callum Gill <callum.gill.1993.cg@gmail.com>
2022-04-15 09:07:15 -0700

    Added the genre page with latest books

--- backend/app/Http/Controllers/BookGenreController.php
+++ backend/app/Http/Controllers/BookGenreController.php
@@ -7,9 +7,17 @@ use App\Models\Genre;
 
 class BookGenreController extends Controller
 {
+    public function index()
+    {
+        return Genre::all();
+    }
+
     public function show(Request $request, $slug)
     {
         $genre = Genre::where(['slug' => $slug])->first();
-        return $genre->books()->limit(10)->get();
+        return [
+            'genre' => $genre,
+            'books' => $genre->books()->limit(10)->get()
+        ];
     }
 }
