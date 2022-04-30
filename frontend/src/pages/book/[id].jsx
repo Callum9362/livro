f04419e26ccfe3e1654f761660ea474d600fddbb
Callum Gill <callum.gill.1993.cg@gmail.com>
2022-04-30 08:17:13 -0700

    Created the book page

--- frontend/src/pages/book/[id].jsx
+++ frontend/src/pages/book/[id].jsx
@@ -1,7 +1,9 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
+import Link from 'next/link'
+import Styles from '@/styles/BookPage.module.css';
 
-const Book = () => {
+const Book = ({ book }) => {
     return (
         <AppLayout
             header={
@@ -11,20 +13,75 @@ const Book = () => {
             }>
 
             <Head>
-                <title>Livro - Book Information - Book</title>
+                <title>Livro - Book Information - {book.title}</title>
             </Head>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                            All the information about a single book will display here.
+
+                            <h1 className="text-3xl">{book.title}</h1>
+
+                            <p>{book.blurb}</p>
+
+                            <div>
+                                {book.genres && book.genres.map((genre, i) =>
+                                    <div className={Styles.genreBtn} key={i}>
+                                        <Link href={`/genre/${genre.slug}`}>
+                                            <a>{genre.name}</a>
+                                        </Link>
+                                    </div>
+                                )}
+                            </div>
+
+                            <div className="my-2 flex justify-between">
+                                <div className="ml-2">
+                                    {book.authors && book.authors.map((author, i) =>
+                                        <div key={i}>
+                                            <p>{author.name}</p>
+                                        </div>
+                                    )}
+                                </div>
+
+                                <div className="mx-2">
+                                    {book.publishers && book.publishers.map((publisher, i) =>
+                                        <div key={i}>
+                                            <p>{publisher.name}</p>
+                                        </div>
+                                    )}
+                                </div>
+
+                                <div className="mr-2">
+                                    {book.editions && book.editions.map((edition, i) =>
+                                        <div key={i}>
+                                            <p>{edition.isbn10}</p>
+                                        </div>
+                                    )}
+                                </div>
+
+                            </div>
+
                         </div>
                     </div>
                 </div>
             </div>
+
+
         </AppLayout>
     )
 }
+
+export async function getServerSideProps(context) {
+    const { id } = context.query;
+    const result = await fetch(`http://localhost:8000/api/books/${id}`);
+    const jsonResult = await result.json();
+
+    return {
+        props: {
+            book: jsonResult
+        }
+    };
+};
 
 export default Book
--- frontend/src/pages/genre/[slug].jsx
+++ frontend/src/pages/genre/[slug].jsx
@@ -8,29 +8,29 @@ const Genre = ({ genre, books }) => {
         <AppLayout
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    { genre.name }
+                    {genre.name}
                 </h2>
             }>
 
             <Head>
-                <title>Livro - Discover { genre.name } </title>
+                <title>Livro - Discover {genre.name} </title>
             </Head>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="p-6 bg-white border-b">
                         <div className="min-h-screen flex items-center justify-center">
-                        { books && books.map((book, i) => 
-                    <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-2">
-                        <p>
-                            <Link href={`/book/${book.id}`}>
-                                <a>
-                                { _.truncate(book.title, { length: 24} ) }
-                                </a>
-                            </Link>
-                        </p>
-                    </div>
-                )}
+                            {books && books.map((book, i) =>
+                                <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-2">
+                                    <p>
+                                        <Link href={`/book/${book.id}`}>
+                                            <a>
+                                                {_.truncate(book.title, { length: 24 })}
+                                            </a>
+                                        </Link>
+                                    </p>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
@@ -39,12 +39,11 @@ const Genre = ({ genre, books }) => {
     )
 }
 
-export async function getServerSideProps(context)
-{
+export async function getServerSideProps(context) {
     const { slug } = context.query;
     const result = await fetch(`http://localhost:8000/api/genres/${slug}`);
     const jsonResult = await result.json();
-    
+
     return {
         props: {
             books: jsonResult.books,
